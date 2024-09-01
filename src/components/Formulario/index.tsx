ea1fa0ae3c80c556158d6e952f06cd40bb14773d
Natalia-Timote <nataliamirian.nt@gmail.com>
2024-08-31 21:34:12 -0300

    selecionando tarefas

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -2,18 +2,29 @@ import React from 'react';
 import Button from '../Button';
 import style from './Formulario.module.scss';
 import { ITarefa } from '../../types/tarefa';
+import { v4 as uuidv4 } from 'uuid';
 
 class Formulario extends React.Component<{
     setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
 }> {
     state = {
         tarefa: "",
-        tempo: "00:00"
+        tempo: "00:00",
     }
     
     adicionarTarefa(evento : React.FormEvent<HTMLFormElement>) {
         evento.preventDefault();
-        this.props.setTarefas(tarefasAntigas => [...tarefasAntigas, { ...this.state }]);
+        this.props.setTarefas(tarefasAntigas => 
+            [
+                ...tarefasAntigas,
+                {
+                    ...this.state,
+                    selecionado: false,
+                    completado: false,
+                    id: uuidv4()
+                }
+            ]
+        );
         this.setState ({
             tarefa: "",
             tempo: "00:00:00"
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -7,12 +7,20 @@ import { ITarefa } from '../types/tarefa';
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+  }
 
   return (
     <div className={style.AppStyle}>
       <h1 className="title">Alura Studies</h1>
       <Formulario setTarefas={setTarefas} />
-      <Lista tarefas={tarefas} />
+      <Lista
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa} 
+      />
       <Cronometro />
     </div>
   );
